9ac79f0fed1c274ddf2c2aa0dffaf46d8075eb64
lyx0206331 <rq880113@163.com>
2021-12-03 19:10:59 +0800

    update main page data

--- app/src/main/java/com/chwishay/orthrecnursing/MainActivity.kt
+++ app/src/main/java/com/chwishay/orthrecnursing/MainActivity.kt
@@ -4,7 +4,6 @@ import android.graphics.Color
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.TextView
 import androidx.annotation.ColorInt
 import androidx.core.view.isVisible
 import com.chwishay.orthrecnursing.views.ExpandableEditText
@@ -16,89 +15,26 @@ import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
 import com.github.mikephil.charting.highlight.Highlight
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener
-import com.github.mikephil.charting.utils.ColorTemplate
 import io.reactivex.android.schedulers.AndroidSchedulers
-import kotlinx.coroutines.*
+import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.find
-import org.w3c.dom.Text
+import kotlin.math.round
 import kotlin.system.exitProcess
 
 class MainActivity : BaseActivity() {
-
-    private lateinit var tvTargetTrainingDuration: TextView
-    private lateinit var tvTrainingDuration: TextView
-    private lateinit var tvTargetTrainingNum: TextView
-    private lateinit var tvTargetJointAngle: TextView
-    private lateinit var tvCurrentTrainingNum: TextView
-    private lateinit var tvCompleteTrainingNum: TextView
-    private lateinit var tvJointAngle: TextView
-    private lateinit var tvJointAngleVelocity: TextView
-    private lateinit var tvLateralFemoralMuscleContractionStrength: TextView
-    private lateinit var tvMedialFemoralMuscleContractionStrength: TextView
-    private lateinit var tvBicepsFemoralContractionStrength: TextView
-    private lateinit var tvSemitendinosusFemoralContractionStrength: TextView
-    private lateinit var tvAnteriorTibialTendonContractionStrength: TextView
-    private lateinit var tvPeronealMuscleContractionStrength: TextView
-    private lateinit var lineChart: LineChart
-
-    private lateinit var tvLog: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        tvLog = findViewById(R.id.tvLog)
-        tvTrainingDuration = findViewById(R.id.tvTrainingDuration)
-        tvTargetTrainingDuration = findViewById<TextView>(R.id.tvTargetTrainingDuration)
-        tvTargetTrainingNum = findViewById<TextView>(R.id.tvTargetTrainingNum)
-        tvTargetJointAngle = findViewById<TextView>(R.id.tvTargetJointAngle)
-        tvCurrentTrainingNum = findViewById<TextView>(R.id.tvCurrentTrainingNum)
-        tvCompleteTrainingNum = findViewById<TextView>(R.id.tvCompleteTrainingNum)
-        tvJointAngle = findViewById<TextView>(R.id.tvJointAngle)
-        tvJointAngleVelocity = findViewById<TextView>(R.id.tvJointAngleVelocity)
-        tvLateralFemoralMuscleContractionStrength = findViewById<TextView>(R.id.tvLateralFemoralMuscleContractionStrength)
-        tvMedialFemoralMuscleContractionStrength = findViewById<TextView>(R.id.tvMedialFemoralMuscleContractionStrength)
-        tvBicepsFemoralContractionStrength = findViewById<TextView>(R.id.tvBicepsFemoralContractionStrength)
-        tvSemitendinosusFemoralContractionStrength = findViewById<TextView>(R.id.tvSemitendinosusFemoralContractionStrength)
-        tvAnteriorTibialTendonContractionStrength = findViewById<TextView>(R.id.tvAnteriorTibialTendonContractionStrength)
-        tvPeronealMuscleContractionStrength = findViewById<TextView>(R.id.tvPeronealMuscleContractionStrength)
-        lineChart = find<LineChart>(R.id.chart).also {
-            it.setOnChartValueSelectedListener(object : OnChartValueSelectedListener {
-                override fun onValueSelected(e: Entry?, h: Highlight?) {
-                }
-
-                override fun onNothingSelected() {
-                }
-            })
-            it.description.isEnabled = true
-            it.setTouchEnabled(true)
-            it.isDragEnabled = true
-            it.setScaleEnabled(true)
-            it.setDrawGridBackground(false)
-            it.setPinchZoom(true)
-            it.setBackgroundColor(Color.LTGRAY)
-            it.data = LineData().apply { setValueTextColor(Color.WHITE) }
-            it.legend.apply {
-                form = Legend.LegendForm.LINE
-                textColor = Color.WHITE
-            }
-            it.xAxis.apply {
-                textColor = Color.WHITE
-                setDrawGridLines(false)
-                setAvoidFirstLastClipping(false)
-                isEnabled = true
-            }
-//            it.axisLeft.apply {
-//                textColor = Color.WHITE
-//                axisMaximum = 40f
-//                axisMinimum = -40f
-//                setDrawGridLines(true)
-//            }
-            //根据数据自动缩放展示最大最小值,不能设置axisLeft,否则自动缩放无效
-            it.isAutoScaleMinMaxEnabled = true
-            it.axisRight.isEnabled = false
-            it.description.isEnabled = false
-        }
+        chartJointAngle.init(getString(R.string.joint_angle_curve))
+        chartJointAngleVelocity.init(getString(R.string.joint_angle_velocity_curve))
+        chartLateralMuscle.init(getString(R.string.lateral_muscle_caurve))
+        chartBiceps.init(getString(R.string.biceps_curve))
+        chartSemitendinosus.init(getString(R.string.semitendinosus_curve))
+        chartMedialMuscle.init(getString(R.string.medial_muscle_curve))
+        chartTibialisAnteriorMuscle.init(getString(R.string.tibialis_anterior_muscle_curve))
+        chartPeroneusLongus.init(getString(R.string.peroneus_longus_curve))
 
         tvLog.also {
             it.isScrollable(true)
@@ -113,35 +49,42 @@ class MainActivity : BaseActivity() {
         }
 
         DispatchUtil.timeCounterLiveData.observe(this) {
-            tvTrainingDuration.text = it
+            tvCurrentTrainingDuration.text = it
         }
 
-        DispatchUtil.onResultObservable().observeOn(AndroidSchedulers.mainThread()).subscribe() {
+        DispatchUtil.onResultObservable().observeOn(AndroidSchedulers.mainThread()).subscribe {
             if (DispatchUtil.isTimerStart) {
+                tvEverydayTrainingDuration.text = "${it.everydayTrainingDuration}m"
+                tvCurrentTrainingNum.text = "${it.currentTrainingNum}次"
+                tvEverydayTrainingGroups.text = "${it.eachGroupTrainingNum}组"
+                tvCurrentTrainingGroups.text =
+                    "${round(1f * it.currentTrainingNum / it.eachGroupTrainingNum)}组"
+                tvGroupTrainingNum.text = "${it.eachGroupTrainingNum}次"
+                tvTargetJointAngle.text = "${it.targetJointAngle}°"
+                tvJointAngle.text = "${it.jointAngle}°"
+                tvJointAngleVelocity.text = "${it.jointAngleVelocity}°/s"
+                tvLateralFemoralMuscleContractionStrength.text = "${it.lateral_femoral_muscle}"
+                tvMedialFemoralMuscleContractionStrength.text = "${it.medial_femoris}"
+                tvBicepsFemoralContractionStrength.text = "${it.biceps_femoris}"
+                tvSemitendinosusFemoralContractionStrength.text = "${it.semitendinosus_femoris}"
+                tvAnteriorTibialTendonContractionStrength.text = "${it.tibialis_anterior_muscle}"
+                tvPeronealMuscleContractionStrength.text = "${it.peroneus_longus}"
+
+                chartJointAngle.addEntry(LineEntry("角度", it.jointAngle.toFloat()))
+                chartJointAngleVelocity.addEntry(LineEntry("角速度", it.jointAngleVelocity.toFloat()))
+                chartLateralMuscle.addEntry(LineEntry("外侧肌", it.lateral_femoral_muscle.toFloat()))
+                chartBiceps.addEntry(LineEntry("二头肌", it.biceps_femoris.toFloat()))
+                chartSemitendinosus.addEntry(LineEntry("半腱肌", it.semitendinosus_femoris.toFloat()))
+                chartMedialMuscle.addEntry(LineEntry("内侧肌", it.medial_femoris.toFloat()))
+                chartTibialisAnteriorMuscle.addEntry(
+                    LineEntry(
+                        "胫前肌",
+                        it.tibialis_anterior_muscle.toFloat()
+                    )
+                )
+                chartPeroneusLongus.addEntry(LineEntry("腓长肌", it.peroneus_longus.toFloat()))
+
 //                "BYTES_VALUE".logE("${it}")
-//                lineChart.addEntry(it)
-//                tvTargetTrainingDuration.text =
-//                    "${it.targetTrainingDuration}min"
-//                tvTargetTrainingNum.text = "${it.targetTrainingNum}次"
-//                tvTargetJointAngle.text = "${it.targetJointAngle}°"
-//                tvCurrentTrainingNum.text = "${it.currentTrainingNum}次"
-//                tvCompleteTrainingNum.text =
-//                    "${it.completeTrainingNum}次"
-//                tvJointAngle.text = "${it.jointAngle}°"
-//                tvJointAngleVelocity.text =
-//                    "${it.jointAngleVelocity}°/s"
-//                tvLateralFemoralMuscleContractionStrength.text =
-//                    "${it.lateralFemoralMuscleContractionStrength}"
-//                tvMedialFemoralMuscleContractionStrength.text =
-//                    "${it.medialFemoralMuscleContractionStrength}"
-//                tvBicepsFemoralContractionStrength.text =
-//                    "${it.bicepsFemoralContractionStrength}"
-//                tvSemitendinosusFemoralContractionStrength.text =
-//                    "${it.semitendinosusFemoralContractionStrength}"
-//                tvAnteriorTibialTendonContractionStrength.text =
-//                    "${it.anteriorTibialTendonContractionStrength}"
-//                tvPeronealMuscleContractionStrength.text =
-//                    "${it.peronealMuscleContractionStrength}"
             }
         }
 
@@ -152,12 +95,87 @@ class MainActivity : BaseActivity() {
                 }
             }
 
-        find<ExpandableEditText>(R.id.eet).onCenterTextChangedListener = { text, start, before, count ->
-            "EET".logE("text:$text, start:$start, before:$before, count:$count")
-        }
+        find<ExpandableEditText>(R.id.eet).onCenterTextChangedListener =
+            { text, start, before, count ->
+                "EET".logE("text:$text, start:$start, before:$before, count:$count")
+            }
     }
 
-    fun LineChart.addEntry(dataInfo: DataInfo) = this.data?.let { d ->
+    private fun LineChart.init(desc: String) {
+        setOnChartValueSelectedListener(object : OnChartValueSelectedListener {
+            override fun onValueSelected(e: Entry?, h: Highlight?) {
+            }
+
+            override fun onNothingSelected() {
+            }
+        })
+        description.isEnabled = true
+        setTouchEnabled(true)
+        isDragEnabled = true
+        setScaleEnabled(true)
+        setDrawGridBackground(false)
+        setPinchZoom(true)
+        setBackgroundColor(Color.LTGRAY)
+        data = LineData().apply { setValueTextColor(Color.WHITE) }
+        legend.apply {
+            form = Legend.LegendForm.LINE
+            textColor = Color.WHITE
+        }
+        xAxis.apply {
+            textColor = Color.WHITE
+            setDrawGridLines(false)
+            setAvoidFirstLastClipping(false)
+            isEnabled = true
+        }
+//            it.axisLeft.apply {
+//                textColor = Color.WHITE
+//                axisMaximum = 40f
+//                axisMinimum = -40f
+//                setDrawGridLines(true)
+//            }
+        //根据数据自动缩放展示最大最小值,不能设置axisLeft,否则自动缩放无效
+        isAutoScaleMinMaxEnabled = true
+        axisRight.isEnabled = false
+        description.text = desc
+    }
+
+    private fun LineChart.addEntry(vararg entries: LineEntry) = this.data?.let { d ->
+        fun getDateSet(index: Int, @ColorInt color: Int, name: String) =
+            d.getDataSetByIndex(index) ?: LineDataSet(null, name).also { lds ->
+                lds.axisDependency = YAxis.AxisDependency.LEFT
+                lds.color = color
+                lds.setCircleColor(Color.WHITE)
+                lds.lineWidth = 1f
+                lds.circleRadius = 2f
+                lds.fillAlpha = 65
+                lds.fillColor = color
+                lds.highLightColor = Color.rgb(244, 177, 177)
+                lds.valueTextColor = Color.WHITE
+                lds.valueTextSize = 9f
+                lds.setDrawCircles(false)
+                d.addDataSet(lds)
+            }
+        entries.forEachIndexed { index, lineEntry ->
+            d.addEntry(
+                Entry(
+                    getDateSet(
+                        index,
+                        lineEntry.color,
+                        lineEntry.name
+                    ).entryCount.toFloat(), lineEntry.value
+                ), index
+            )
+        }
+        d.notifyDataChanged()
+
+        this.notifyDataSetChanged()
+
+        this.setVisibleXRangeMaximum(1000f)
+
+        this.moveViewToX(d.entryCount.toFloat())
+    }
+
+    fun LineChart.addEntry2(dataInfo: DataInfo) = this.data?.let { d ->
         fun getDateSet(index: Int, @ColorInt color: Int, name: String) =
             d.getDataSetByIndex(index) ?: LineDataSet(null, name).also { lds ->
                 lds.axisDependency = YAxis.AxisDependency.LEFT
@@ -249,3 +267,5 @@ class MainActivity : BaseActivity() {
         exitProcess(0)
     }
 }
+
+class LineEntry(val name: String, val value: Float, @ColorInt val color: Int = Color.BLACK)
